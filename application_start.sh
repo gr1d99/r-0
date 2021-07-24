ea2ad12e7936b026f761242c5722c2cfb7d25e7e
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2021-07-24 06:13:35 +0300

    add run command

--- application_start.sh
+++ application_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd /home/r-0-user && chown -R $USER . && /usr/bin/npm install --loglevel verbose && /usr/bin/npm build --loglevel verbose
+cd /home/r-0-user && chown -R $USER . && /usr/bin/npm install --loglevel verbose && /usr/bin/npm run build --loglevel verbose
