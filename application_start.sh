40fb97dcea1868f0b3662cf2ee2ffad4a2d9162d
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2021-07-24 05:51:15 +0300

    verbose npm output

--- application_start.sh
+++ application_start.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-cd /home/r-0-user/ || return
-
-chown -R $USER .
-npm install
-npm build
+cd /home/r-0-user && chown -R $USER . && /usr/bin/npm install --loglevel verbose && /usr/bin/npm build --loglevel verbose
